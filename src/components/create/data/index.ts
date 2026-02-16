8d206e82402c0ca24af09e7616836cb88ba91b54
mozdev <mazosmann@gmail.com>
2026-02-16 00:52:22 -0500

    Fix styling: use BaseLayout, remove Bearnie branding, add Decap CMS, JSON data files

--- src/components/create/data/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export all theme data
-export { baseColorOptions, baseColorValues } from './base-colors';
-export { primaryColorOptions, primaryColorValues } from './primary-colors';
-export { radiusOptions, radiusNames } from './radius';
-export { fontOptions, fontFamilies } from './fonts';
-export { styleOptions, stylePresets } from './styles';
-export { iconLibraryOptions, iconLibraryPackages } from './icon-libraries';
